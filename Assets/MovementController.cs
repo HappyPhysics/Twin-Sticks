f6b7aced5e2507bba3ed20dbe369b76bfb71c665
DESKTOP-O8MDLOJ\Salem <alemsalem@gmail.com>
2017-02-11 15:15:41 -0500

    playing with cross platform input for the first time.

---
+++ Assets/MovementController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class MovementController : MonoBehaviour {
+	private float bulletRate = 5.0f;
+
+	private float bulletTimer = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis ("Horizontal") != 0) {
+			print(Input.GetAxis ("Horizontal"));
+		}
+
+		if (Input.GetButton ("Fire1")) {
+			bulletTimer += Time.deltaTime;
+
+			if (bulletTimer > 1.0f / bulletRate) {
+				print("fire");
+				bulletTimer = 0;
+			} 
+		} 
+	}
+}
